0357e00c36a8df81239db859a6bd2d28a453458f
Andrea Pavan <andreapavan89@gmail.com>
2016-01-18 14:37:04 +0100

    created project and added test functions file

---
+++ test.erl
@@ -0,0 +1,34 @@
+-module(examples).
+-export([fac/1, average/1, double/1, member/2, averageAcc/1, allsame/1]).
+
+fac(0) -> 1;
+fac(N) -> N * fac(N-1).
+
+% Common functional patterns
+
+average(X) -> sum(X) / len(X).
+	
+sum([H|T]) -> H + sum(T);
+sum([]) -> 0.
+	
+len([_|T]) -> 1 + len(T);
+len([]) -> 0.
+
+double([H|T]) -> [2*H|double(T)];
+double([]) -> [].
+	
+member(H, [H|_]) -> true;
+member(H, [_|T]) -> member(H, T);
+member(_, []) -> false.
+
+% Average with accumulators
+
+averageAcc(X) -> average(X, 0, 0).
+average([H|T], Length, Sum) -> average(T, Length + 1, Sum + H);
+average([], Length, Sum) -> Sum / Length.
+
+% Other examples
+
+allsame([]) -> true;
+allsame([_]) -> true;
+allsame([H,H|T]) -> allsame([H|T]). 
