b718a0d24a96f77f812fbebd954fb880762e857a
Daniele Canciani <croma25td@gmail.com>
2016-02-11 23:13:08 +0100

    exception handling

--- riak.erl
+++ riak.erl
@@ -7,20 +7,36 @@ start() ->
 	{ok, Pid} = riakc_pb_socket:start_link("127.0.0.1", 10017).
 
 setObject(Bucket, Key, Value) ->
-	{ok, Pid} = riakc_pb_socket:start_link("127.0.0.1", 10017),
-	Obj = riakc_obj:new(atom_to_binary(Bucket, latin1), atom_to_binary(Key, latin1), atom_to_binary(Value, latin1)),
-	riakc_pb_socket:put(Pid, Obj).
+	try
+		{ok, Pid} = riakc_pb_socket:start_link("127.0.0.1", 10017),
+		Obj = riakc_obj:new(atom_to_binary(Bucket, latin1), atom_to_binary(Key, latin1), atom_to_binary(Value, latin1)),
+		riakc_pb_socket:put(Pid, Obj)
+	catch
+		Exception:Reason -> {caught, Exception, Reason}
+	end.
 
 getObject(Bucket, Key) ->
-	{ok, Pid} = riakc_pb_socket:start_link("127.0.0.1", 10017),
-	{ok, Obj} = riakc_pb_socket:get(Pid, atom_to_binary(Bucket, latin1), atom_to_binary(Key, latin1)).
+	try
+		{ok, Pid} = riakc_pb_socket:start_link("127.0.0.1", 10017),
+		{ok, Obj} = riakc_pb_socket:get(Pid, atom_to_binary(Bucket, latin1), atom_to_binary(Key, latin1))
+	catch
+		Exception:Reason -> {caught, Exception, Reason}
+	end.
 
 deleteObject(Bucket, Key) ->
-	{ok, Pid} = riakc_pb_socket:start_link("127.0.0.1", 10017),
-	riakc_pb_socket:delete(Pid, atom_to_binary(Bucket, latin1), atom_to_binary(Key, latin1)).
+	try
+		{ok, Pid} = riakc_pb_socket:start_link("127.0.0.1", 10017),
+		riakc_pb_socket:delete(Pid, atom_to_binary(Bucket, latin1), atom_to_binary(Key, latin1))
+	catch
+		Exception:Reason -> {caught, Exception, Reason}
+	end.
 
 updateObject(Bucket, Key, NewValue) ->
-	{ok, Pid} = riakc_pb_socket:start_link("127.0.0.1", 10017),
-	{ok, Oa} = riakc_pb_socket:get(Pid, atom_to_binary(Bucket, latin1), atom_to_binary(Key, latin1)),
-	Ob = riakc_obj:update_value(Oa, atom_to_binary(NewValue, latin1)),
-	{ok, Oc} = riakc_pb_socket:put(Pid, Ob, [return_body]).
+	try
+		{ok, Pid} = riakc_pb_socket:start_link("127.0.0.1", 10017),
+		{ok, Oa} = riakc_pb_socket:get(Pid, atom_to_binary(Bucket, latin1), atom_to_binary(Key, latin1)),
+		Ob = riakc_obj:update_value(Oa, atom_to_binary(NewValue, latin1)),
+		{ok, Oc} = riakc_pb_socket:put(Pid, Ob, [return_body])
+	catch
+		Exception:Reason -> {caught, Exception, Reason}
+	end.
