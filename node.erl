090e97fc99341d947b050460b642aa078fa965f6
Andrea Pavan <andreapavan89@gmail.com>
2016-02-12 22:27:59 +0100

    added node module

---
+++ node.erl
@@ -0,0 +1,53 @@
+-module(node).
+-behaviour(gen_server).
+
+%% Export node functions
+-export([connect/0, disconnect/0, newWorker/2, startJob/2, completejob/2, cancelJob/2]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+-compile(export_all).
+
+connect() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+disconnect() -> gen_server:call(?MODULE, stop).
+
+newWorker(Who,JobId) -> gen_server:call(?MODULE, {new,Who,JobId}).
+cancelJob(Who,JobId) -> gen_server:call(?MODULE, {cancel,Who,JobId}).
+startJob(Who,JobId) -> gen_server:call(?MODULE, {start,Who,JobId}).
+completejob(Who,JobId) -> gen_server:call(?MODULE, {complete,Who,JobId}).
+
+init([]) -> {ok, ets:new(?MODULE,[])}.
+
+handle_call({new,Who,_JobId}, From, Tab) ->
+	Reply = case ets:lookup(Tab, Who) of
+		[]  -> ets:insert(Tab, {Who,0}), 
+		       {welcome, Who, From};
+		[_] -> {Who, you_already_are_a_worker}
+	    end,
+    {reply, Reply, Tab};
+
+handle_call({cancel,Who,_JobId}, From, Tab) ->
+	Reply = case ets:lookup(Tab, Who) of
+		[_] -> ets:delete(Tab, Who),
+				{thanks_for_the_work, Who};
+		[]  ->  {you_are_not_a_worker, Who}
+	    end,
+    {reply, Reply, Tab};
+
+handle_call({start,Who,_JobId}, From, Tab) ->
+	Reply = {start, Who, From},
+    {reply, Reply, Tab};
+
+handle_call({complete,Who,_JobId}, From, Tab) ->
+	Reply = {complete, Who, From},
+    {reply, Reply, Tab};
+
+handle_call(stop, _From, Tab) ->
+    {stop, normal, stopped, Tab}.
+
+
+handle_cast(_Msg, State) -> {noreply, State}.
+handle_info(_Info, State) -> {noreply, State}.
+terminate(_Reason, _State) -> ok.
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
