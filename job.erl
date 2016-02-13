2ceb94621a0acc614cf8bf17bccb454c68930233
Daniele Canciani <croma25td@gmail.com>
2016-02-13 16:34:22 +0100

    addJob, listJob and getJobDetail

--- job.erl
+++ job.erl
@@ -1,5 +1,5 @@
 -module(job).
--export([addJob/5]).
+-export([addJob/5, listJob/0, getJobDetail/1]).
 
 -record(job_info,
 	{owner="",
@@ -14,9 +14,27 @@
 addJob(Owner, Core, Ram, Disk, JobCost) ->
 	try
 		JobInfo1 = #job_info{owner=Owner, core=Core, ram=Ram, disk=Disk, job_cost=JobCost},
-		riak:setObject("Job", undefined, term_to_binary(JobInfo1))
+		riak:setObject('Job', undefined, term_to_binary(JobInfo1))
 	catch
 		Exception:Reason -> {caught, Exception, Reason}
 	end.
 
+% listJob()
+listJob() ->
+	try
+		riak:listObject('Job')
+	catch
+		Exception:Reason -> {caught, Exception, Reason}
+	end.
+
+% getJobDetail(Key)
+% {Key} as Binary
+getJobDetail(Key) ->
+	try
+		{ok, Obj} = riak:getObject('Job', Key),
+		Obj2 = binary_to_term(riakc_obj:get_value(Obj))
+
+	catch
+		Exception:Reason -> {caught, Exception, Reason}
+	end.
 
