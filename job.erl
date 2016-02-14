ac5e1de465223b0ec6fed490cbdd72477f245d23
Daniele Canciani <croma25td@gmail.com>
2016-02-14 17:13:17 +0100

    added new field on job_info record and added updateJobStatus function

--- job.erl
+++ job.erl
@@ -1,19 +1,21 @@
 -module(job).
--export([addJob/5, listJob/0, getJobDetail/1]).
+-export([addJob/6, listJob/0, getJobDetail/1, updateJobStatus/2]).
 
 -record(job_info,
-	{owner="",
+	{status="",
+	owner="",
 	core=1,
 	ram=1,
 	disk=1,
 	job_cost=1}).
 
-% addJob(Owner, Core, Ram, Disk, JobCost)
+% addJob(Status, Owner, Core, Ram, Disk, JobCost)
+% {Status} as String
 % {Owner} as Node (ex. 'node@machine')
 % {Core, Ram, Disk, JobCost} as integer
-addJob(Owner, Core, Ram, Disk, JobCost) ->
+addJob(Status, Owner, Core, Ram, Disk, JobCost) ->
 	try
-		JobInfo1 = #job_info{owner=Owner, core=Core, ram=Ram, disk=Disk, job_cost=JobCost},
+		JobInfo1 = #job_info{status=Status, owner=Owner, core=Core, ram=Ram, disk=Disk, job_cost=JobCost},
 		riak:setObject('Job', undefined, term_to_binary(JobInfo1))
 	catch
 		Exception:Reason -> {caught, Exception, Reason}
@@ -33,7 +35,18 @@ getJobDetail(Key) ->
 	try
 		{ok, Obj} = riak:getObject('Job', Key),
 		Obj2 = binary_to_term(riakc_obj:get_value(Obj))
+	catch
+		Exception:Reason -> {caught, Exception, Reason}
+	end.
 
+% updateJobStatus(Key, Status)
+% {Key} as Binary
+% {Status} as String
+updateJobStatus(Key, Status) ->
+	try
+		Obj = job:getJobDetail(Key),
+		UpdJpb = #job_info{status=Status, owner=Obj#job_info.owner, core=Obj#job_info.core, ram=Obj#job_info.ram, disk=Obj#job_info.disk, job_cost=Obj#job_info.job_cost},
+		riak:updateObject('Job', Key, term_to_binary(UpdJpb))
 	catch
 		Exception:Reason -> {caught, Exception, Reason}
 	end.
