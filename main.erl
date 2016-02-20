f870914af9ff6035f9f67eb37f7faca2de8f3a45
Daniele Canciani <croma25td@gmail.com>
2016-02-20 15:43:55 +0100

    working on integration

--- main.erl
+++ main.erl
@@ -1,17 +1,35 @@
 -module(main).
--export([join/0, submitJob/4, monitorNode/1, startJob/0, cleanDHT/0]).
+-export([join/4, submitJob/4, checkJob/1, startJob/0, cleanDHT/0]).
 
 
-join()-> 
-	[RiakAddress,RiakPort] = init:get_plain_arguments(),
-	{NewPort, _} = string:to_integer(RiakPort),
-	riak:start(RiakAddress, NewPort),
-	work:start().
+% join(Core, Ram, Disk, Price)
+% joins the job scheduling network by registering the node
+% {Core, Ram, Disk, Price} as integer
+join(Core, Ram, Disk, Price)-> 
+	% connect to riak
+	connect(),
+	% register node to riak (status is ready by default)
+	node:addNode(node(), "ready", Core, Ram, Disk, Price),
+	% register node name as worker
+	work:start(),
+	io:format("Benvenuto ~p ~n", [node()]).
 
-submitJob(_Core, _Ram, _Disk, _JobCost)-> {ok}.
-	%%job:addJob('andrea2@home', Core, Ram, Disk, JobCost).
+submitJob(Core, Ram, Disk, JobCost)-> 
+	% register a new job to riak (status is ready by default)
+	{_, {_, _, JobKey, _, _, _, _}} = job:addJob("ready", node(), Core, Ram, Disk, JobCost),
+	% check if there is a node able to run the new job
+	checkJob(JobKey).
 
-monitorNode(NodeName)->
+checkJob(JobKey) ->
+	% check if there is a node able to run an existing job
+	Node = policy:computeWorker(JobKey),
+	monitorNode(binary_to_atom(Node, latin1), JobKey).
+
+monitorNode(null, JobKey) -> 
+	io:format("Nessun nodo attulmente disponibile ~nRiprova piu tardi jobKey: ~p", [JobKey]),
+	JobKey;
+
+monitorNode(NodeName, _JobKey)->
 	% Spawn a new process to receive the message from monitoring
 	spawn(fun()->
 		erlang:monitor_node(NodeName, true),
@@ -23,11 +41,27 @@ monitorNode(NodeName)->
 		end
 	end).
 
+
 startJob() ->
 	io:format("Job iniziato~n", []).
 
 % cleanDHT()
 % cleans the DHT from all the values, used only for DEBUG purposes
 cleanDHT() ->
+	% connect to riak
+	connect(),
+	% clean all jobs
 	job:cleanJob(),
+	% clean all nodes
 	node:cleanNode().
+
+connect() ->
+	try
+		% get extra parameters for riak connection: address and port
+		[RiakAddress,RiakPort] = init:get_plain_arguments(),
+		{NewPort, _} = string:to_integer(RiakPort),
+		% connection to riak node
+		riak:start(RiakAddress, NewPort)
+	catch
+		Exception:Reason -> {caught, Exception, Reason}
+	end.
--- policy.erl
+++ policy.erl
@@ -32,14 +32,15 @@ if
 	CurNode#node_info.core >= Job#job_info.core,
 	CurNode#node_info.ram >= Job#job_info.ram,
 	CurNode#node_info.disk >= Job#job_info.disk ->
-		verifyNode(Job, T, [CurNode|ValidNodes]);
+		verifyNode(Job, T, [H|ValidNodes]);
 	true ->
 		verifyNode(Job, T, ValidNodes)
 end;
 verifyNode(_, [], ValidNodes) -> ValidNodes.
 
-findBestBidder([H|T]) -> findBestBidder(H,T).
+findBestBidder([H|T]) -> findBestBidder(H,T);
+findBestBidder([]) -> null.
 
-findBestBidder(M,[H|L]) when M#node_info.price < H#job_info.job_cost -> findBestBidder(M,L);
+findBestBidder(M,[H|L]) when M#node_info.price < H#node_info.price -> findBestBidder(M,L);
 findBestBidder(_,[H|L]) -> findBestBidder(H,L);
 findBestBidder(M,[]) -> M.
